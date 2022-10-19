04ad391ef1bcecf1e1a6844fc8e6ddbae9919109
Etienne Stock <etienne.stock@web.de>
2022-10-19 17:23:19 +0200

    Added dictionary for spin decoder.

--- const.py
+++ const.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 
+# decode axis
+ax_ind = {}
+ax_ind['x'] = 1
+ax_ind['y'] = 2
+ax_ind['z'] = 3
+
+# decode spin
+spin_ind = {}
+spin_ind[1]  = 1
+spin_ind[-1] = 0
+
 # Pauli matrices
 se = np.eye(2)
 sx = np.array([[0, 1],
@@ -14,22 +25,26 @@ spovm = np.array([se, sx, sy, sz])
 
 
 # Pauli eigenstates
-estate = {}
-estate[(1, +1)] = 1/np.sqrt(2)*np.array([1, 1])
-estate[(1, -1)] = 1/np.sqrt(2)*np.array([1, -1])
-estate[(2, +1)] = 1/np.sqrt(2)*np.array([1, 1j])
-estate[(2, -1)] = 1/np.sqrt(2)*np.array([1, -1j])
-estate[(3, +1)] = np.array([1, 0])
-estate[(3, -1)] = np.array([0, 1])
+estate = np.empty((4, 2, 2), dtype=complex)
+estate[0, 1] = np.array([1, 0])
+estate[0, 0] = np.array([0, 1])
+estate[1, 1] = 1/np.sqrt(2)*np.array([1, 1])
+estate[1, 0] = 1/np.sqrt(2)*np.array([1, -1])
+estate[2, 1] = 1/np.sqrt(2)*np.array([1, 1j])
+estate[2, 0] = 1/np.sqrt(2)*np.array([1, -1j])
+estate[3, 1] = np.array([1, 0])
+estate[3, 0] = np.array([0, 1])
 
 # Pauli eigenstates in density representation
-edensity = {}
-edensity[(1, +1)] = 1/2*np.array([[1, 1], [1, 1])
-edensity[(1, -1)] = 1/2*np.array([[1, -1], [-1, 1]])
-edensity[(2, +1)] = 1/2*np.array([[1, 1j], [-1j, 1]])
-edensity[(2, -1)] = 1/2*np.array([[1, 1j], [-1j, 1]])
-edensity[(3, +1)] = np.array([[1, 0], [0, 0]])
-edensity[(3, -1)] = np.array([[0, 0], [0, 1]])
+edensity = np.empty((4, 2, 2, 2), dtype=complex)
+edensity[0, 1] = np.array([[0.5, 0], [0, 0.5]])
+edensity[0, 0] = np.array([[0.5, 0], [0, 0.5]])
+edensity[1, 1] = 1/2*np.array([[1, 1], [1, 1]])
+edensity[1, 0] = 1/2*np.array([[1, -1], [-1, 1]])
+edensity[2, 1] = 1/2*np.array([[1, -1j], [1j, 1]])
+edensity[2, 0] = 1/2*np.array([[1, 1j], [-1j, 1]])
+edensity[3, 1] = np.array([[1, 0], [0, 0]])
+edensity[3, 0] = np.array([[0, 0], [0, 1]])
 
 
 # rotation matrices
