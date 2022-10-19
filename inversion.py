01338ae337d9518fdfc01c4dc2a322cb505c9c79
Etienne Stock <etienne.stock@web.de>
2022-10-19 17:26:48 +0200

    Adjust linear inversion to dataype of measurements.

--- inversion.py
+++ inversion.py
@@ -1,16 +1,36 @@
 import numpy as np
 
 
-def linear(M: np.array, D: np.array):
+def count(D: np.array, n: np.array):
+    '''
+    Iterative counter for linear inversion.
+
+    :param D: datatype: array of [axes, measured eigenvalue]
+    :param n: list with number of spin up results and number of spin down reults
+        for corresponding POVM
+    :result: updated n
+    '''
+    J = len(n)
+    I = np.concatenate(np.transpose(np.meshgrid(np.arange(J), [0, 1]), axes=[2, 1, 0]))
+
+    for i in I:
+        n[tuple(i)] += np.sum(np.all(D==i, axis=1))
+
+    return n
+
+
+def linear(n: np.array, M: np.array):
     '''
     Calculates the best estimator according to the theory of linear inversion.
 
+    :param n: list with number of spin up results and number of spin down reults
+        for corresponding POVM
     :param M: set of POVM
-    :param D: len(M)xNx2 dimensional array of measured frequencies for each POVM in M,
-        N describes the number of measurements for each basis
     :return: the linear inversion estimator
     '''
-    p     = np.mean(D[:, :, 1], axis=1)
+    N     = np.sum(n, axis=1)
+    p     = (n[:,1] - n[:,0])/N
+    M     = np.array(M)
     T     = np.einsum('alk,bkl->ab', M, M)
     T_inv = np.linalg.inv(T)
 
