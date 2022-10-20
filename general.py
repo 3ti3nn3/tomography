d1cc9860c1bba9b6472d9ca99ce256fd22efd84c
Etienne Stock <etienne.stock@web.de>
2022-10-20 11:59:45 +0200

    Added expect_xyz.

--- general.py
+++ general.py
@@ -1,4 +1,6 @@
 import numpy as np
+import const
+
 
 def expect(operator: np.array, rho: np.array):
     '''
@@ -9,6 +11,16 @@ def expect(operator: np.array, rho: np.array):
     :return: expectation value
     '''
     return np.trace(operator@rho, axis1=-2, axis2=-1)
+
+
+def expect_xyz(rho: np.array):
+    '''
+    Determines the sigma_x, sigma_y and sigma_z expectation value.
+
+    :param rho: state in density matrix representation
+    :return: array of the three expectation values
+    '''
+    return np.array([expect(const.sx, rho), expect(const.sy, rho), expect(const.sz, rho)], dtype=np.float)
 
 
 def hilbert_schmidt_distance(rho1: np.array, rho2: np.array):
