3060966f55e00e4d04fa1db5dbd2ba5e581a7ea8
Etienne Stock <etienne.stock@web.de>
2022-10-25 17:35:33 +0200

    Adjusted to new datatype and generalized to arbitrary POVMs.

--- mle.py
+++ mle.py
@@ -2,35 +2,58 @@ import numpy as np
 import const
 
 
-def iterative_one(rho: np.array, R_arr: np.array):
+def iterative_iter(rho: np.array, M_D: np.array):
     '''
     Performs one step of iterative MLE according to given measurement.
 
-    :param rho  : current numerical approximation of the estimator
-    :param R_arr: array of eigenstate matrices
+    :param rho: current numerical approximation of the estimator
+    :param M_D: array of eigenstate matrices
     :return: updated estimator
     '''
-    p_arr  = np.einsum('ik,nki->n', rho, R_arr)
-    R      = np.einsum('n,nij->ij', 1/p_arr, R_arr)
+    p = np.einsum('ik,nki->n', rho, M_D)
+    R = np.einsum('n,nij->ij', 1/p, M_D)
     update = R@rho@R
 
     return update/np.trace(update)
 
 
-def iterative(D: np.array, iter: int):
+def iterative(D: np.array, M: np.array, iter=500):
     '''
     Performs iterative MLE.
 
-    :param D   : measured data set, datatype: list of [axis, measured eigenvalue]
+    :param D   : measured data set
+        datatype: D[i] = [index of POVM]
+    :param M   : set of POVMs
     :param iter: number of iterations
     :return: iterative MLE estimator
     '''
-    I     = len(D)
-    rho   = np.eye(2)/2
-    R_arr = np.array([const.edensity[tuple(d)] for d in D])
+    M_D = M[D]
+    rho = np.eye(2)/2
 
-    # perform updates
-    for i in range(iter):
-        rho = iterative_one(rho, R_arr)
+    for j in range(iter):
+        rho = iterative_iter(rho, M_D)
+
+    return rho
+
+
+def iterative_one(D: np.array, M: np.array, iter: int):
+    '''
+    Performs iterative MLE and stores the result after every new data point.
+
+    :param D   : measured data set
+        datatype: D[i] = [index of POVM]
+    :param M   : set POVMs
+    :param iter: number of iterations
+    :return: development of iterative MLE estimator
+    '''
+    N   = len(D)
+    M_D = M[D]
+    rho = np.empty((N, 2, 2), dtype=np.complex)
+
+    for i in range(N):
+        rho_update = np.eye(2)/2
+        for j in range(iter):
+            rho_update = iterative_iter(rho_update, M_D[:i])
+        rho[i] = rho_update
 
     return rho
