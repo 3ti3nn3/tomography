7baea933adc3453ff225de5ee2d473884d104145
Etienne Stock <etienne.stock@web.de>
2022-10-26 18:20:41 +0200

    Generalized infidelity plot.

--- visualization.py
+++ visualization.py
@@ -14,7 +14,7 @@ def qubit(points=np.array([None]), states=np.array([None]), kind='point', angles
     '''
     Depending on the parameters the function creates vectors or points on the Bloch sphere.
 
-    :param points: Nx3 array of expectations values (<sigma_x>, <sigma_y>, <sigma_z>)
+    :param points: Nx2x2 of states in density representation
     :param states: list of N Quatum objects
     :param kind  : str which should be either "point" or "vector"
     :param angles: list of angles from which the Bloch sphere is seen
@@ -28,7 +28,10 @@ def qubit(points=np.array([None]), states=np.array([None]), kind='point', angles
     b.view         = angles
 
     if np.all(points != None):
-        b.add_points(points)
+        try:
+            b.add_points(general.expect_xyz(points).T)
+        except:
+            b.add_points(general.expect_xyz(points))
         b.render()
     if np.all(states != None):
         b.add_states(states, kind=kind)
@@ -42,7 +45,7 @@ def qubit_3(rho_0: (str, np.array), rho_1=(None, np.array([None])), rho_2=(None,
 
     :param rho_1: first state in density representation, dataype: (name, state)
     :param rho_2: second state in density representation, dataype: (name, array of states)
-    :param rho_3: third state in densitu representation, dataype: (name, array of states)
+    :param rho_3: third state in density representation, dataype: (name, array of states)
     '''
     b              = qt.Bloch()
     b.point_marker = 'o'
@@ -246,83 +249,7 @@ def hilbert_dist_iter(rho_0: np.array, M:np.array, iter_max: int, N=1000):
     qubit_3(('Original', rho_0), ('MLE', rho_mle))
 
 
-def bures_dist(rho_0: np.array, M: np.array, N_max: int, iter=50):
-    '''
-    Plots the N dependency of Bures distance.
-
-    :param rho_0 : state to be reconstrected
-    :param M     : set of POVMs
-    :param N_max : maximal N
-    :param iter  : number of iteration needed for the maximum likelihood method
-    :return: N-bures_dist plots for mle and linear inversion
-    '''
-    dim      = rho_0.shape[0]
-    steps    = np.logspace(0, np.log(N_max), 20, dtype=np.int64)
-    n_steps  = len(steps)
-
-    rho_mle  = np.empty((n_steps, dim, dim), dtype=np.complex)
-    rho_inv  = np.empty((n_steps, dim, dim), dtype=np.complex)
-    bur_mle  = np.empty(n_steps, dtype=np.float)
-    bur_inv  = np.empty(n_steps, dtype=np.float)
-
-    D = simulate.measure(rho_0, N_max, M)
-
-    # calculate data points for plots
-    for i in range(n_steps):
-        print(i)
-
-        rho_1      = mle.iterative(D[:steps[i]], M, iter=iter)
-        rho_mle[i] = rho_1
-        bur_mle[i] = general.bures_dist(rho_0, rho_1)
-        print('Maximum Likelihood estimate done!')
-
-        rho_2      = inversion.linear(D[:steps[i]], M)
-        rho_inv[i] = rho_2
-        try:
-            bur_inv[i] = general.bures_dist(rho_0, rho_2)
-        except:
-            bur_inv[i] = 1-general.fidelity(rho_0, rho_2)
-        print('Linear Inversion done!')
-
-    # plots
-    fig, axs = plt.subplots(1, 3, figsize=(30, 5))
-
-    fig.suptitle('N-scaling of Bures distance')
-
-    axs[0].plot(steps, bur_mle, c='blue')
-    axs[0].plot(steps, bur_inv, c='violet')
-    axs[1].plot(steps, bur_mle, c='blue')
-    axs[2].plot(steps, bur_inv, c='violet')
-
-    axs[0].set_title('Comparison')
-    axs[1].set_title('Maximum likelihood estimate')
-    axs[2].set_title('Linear inversion')
-
-    axs[0].set_xlabel(r'$N$')
-    axs[1].set_xlabel(r'$N$')
-    axs[2].set_xlabel(r'$N$')
-
-    axs[0].set_xlim(1, N_max)
-    axs[1].set_xlim(1, N_max)
-    axs[2].set_xlim(1, N_max)
-
-    axs[0].set_xscale('log')
-    axs[1].set_xscale('log')
-    axs[2].set_xscale('log')
-
-    axs[0].set_yscale('log')
-    axs[1].set_yscale('log')
-    axs[2].set_yscale('log')
-
-    axs[0].set_ylabel('Bures distance')
-
-    plt.show()
-
-    # show developmemt of the
-    qubit_3(('Original', rho_0), ('MLE', rho_mle))
-
-
-def infidelity(rho_0: np.array, func1, func2, M1: np.array, M2: np.array, N_max: int):
+def infidelity(rho_0: np.array, func1, func2, M1: np.array, M2: np.array, N_max: int, n_mean: int, q=False):
     '''
     Plots the N dependency of infidelity.
 
@@ -334,42 +261,45 @@ def infidelity(rho_0: np.array, func1, func2, M1: np.array, M2: np.array, N_max:
     :param M1    : set of POVMs for func1
     :param M2    : set of POVMs for func2
     :param N_max : maximal N
+    :param n_mean: number of mean measurements for mean value
+    :param q     : boolean about whether the development should be plotted
     :return: N-bures_dist plots for mle and linear inversion
     '''
     dim      = rho_0.shape[0]
-    steps    = np.logspace(0, np.log(N_max), 20, dtype=np.int64)
+    steps    = np.logspace(2, np.log10(N_max), 10, dtype=np.int64)
     n_steps  = len(steps)
 
-    rho_m1  = np.zeros((n_steps, dim, dim), dtype=np.complex)
-    rho_m2  = np.zeros((n_steps, dim, dim), dtype=np.complex)
-    inf_m1  = np.zeros(n_steps, dtype=np.float)
-    inf_m2  = np.zeros(n_steps, dtype=np.float)
+    rho_m1  = np.zeros((n_mean, n_steps, dim, dim), dtype=np.complex)
+    rho_m2  = np.zeros((n_mean, n_steps, dim, dim), dtype=np.complex)
+    inf_m1  = np.zeros((n_mean, n_steps), dtype=np.float)
+    inf_m2  = np.zeros((n_mean, n_steps), dtype=np.float)
 
-    if np.all(M1==M2):
-        D  = simulate.measure(rho_0, N_max, M1)
-        D1 = D
-        D2 = D
-    else:
-        D1 = simulate.measure(rho_0, N_max, M1)
-        D2 = simulate.measure(rho_0, N_max, M2)
+    for j in range(n_mean):
+        if np.all(M1==M2):
+            D  = simulate.measure(rho_0, N_max, M1)
+            D1 = D
+            D2 = D
+        else:
+            D1 = simulate.measure(rho_0, N_max, M1)
+            D2 = simulate.measure(rho_0, N_max, M2)
 
-    # calculate data points for plots
-    for i in range(n_steps):
-        rho_m1[i] = func1[1](D1[:steps[i]], M1)
-        inf_m1[i] = 1-general.fidelity(rho_0, rho_m1[i])
+        # calculate data points for plots
+        for i in range(n_steps):
+            rho_m1[j,i] = func1[1](D1[:steps[i]], M1)
+            inf_m1[j,i] = 1-general.fidelity(rho_0, rho_m1[j,i])
 
-        rho_m2[i] = func2[1](D2[:steps[i]], M2)
-        inf_m2[i] = 1-general.fidelity(rho_0, rho_m2[i])
+            rho_m2[j,i] = func2[1](D2[:steps[i]], M2)
+            inf_m2[j,i] = 1-general.fidelity(rho_0, rho_m2[j,i])
 
     # plots
     fig, axs = plt.subplots(1, 3, figsize=(30, 5))
 
     fig.suptitle('N-scaling of infidelity')
 
-    axs[0].plot(steps, inf_m1, c='blue', label=func1[0])
-    axs[0].plot(steps, inf_m2, c='violet', label=func2[0])
-    axs[1].plot(steps, inf_m1, c='blue')
-    axs[2].plot(steps, inf_m2, c='violet')
+    axs[0].plot(steps, np.mean(inf_m1, axis=0), c='blue', label=func1[0])
+    axs[0].plot(steps, np.mean(inf_m2, axis=0), c='violet', label=func2[0])
+    axs[1].plot(steps, np.mean(inf_m1, axis=0), c='blue')
+    axs[2].plot(steps, np.mean(inf_m2, axis=0), c='violet')
 
     axs[0].set_title('Comparison')
     axs[1].set_title(func1[0])
@@ -379,27 +309,31 @@ def infidelity(rho_0: np.array, func1, func2, M1: np.array, M2: np.array, N_max:
     axs[1].set_xlabel(r'$N$')
     axs[2].set_xlabel(r'$N$')
 
-    axs[0].set_xlim(1, N_max)
-    axs[1].set_xlim(1, N_max)
-    axs[2].set_xlim(1, N_max)
+    # axs[0].set_xlim(100, N_max)
+    # axs[1].set_xlim(100, N_max)
+    # axs[2].set_xlim(100, N_max)
 
     axs[0].set_xscale('log')
     axs[1].set_xscale('log')
     axs[2].set_xscale('log')
 
-    if func1[1]!=inversion.linear and func2[1]!=inversion.linear:
+    if np.all(inf_m1>0) and np.all(inf_m2>0):
         axs[0].set_yscale('log')
         axs[1].set_yscale('log')
         axs[2].set_yscale('log')
-    elif func1[1]==inversion.linear:
-        axs[2].set_yscale('log')
-    elif func2[1]==inversion.linear:
+    elif np.all(inf_m1>0):
         axs[1].set_yscale('log')
+    elif np.all(inf_m2>0):
+        axs[2].set_yscale('log')
 
     axs[0].set_ylabel('infidelity')
     axs[0].legend()
 
     plt.show()
+
+    # show developmemt of the
+    if q:
+        qubit_3(('Original', rho_0), ('Method 2', rho_m1), ('Method 2', rho_m2))
 
 
 def speed_comparison(title, iterations=10, **kwargs):
