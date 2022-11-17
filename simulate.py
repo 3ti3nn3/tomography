c086d3bf5755e62c473fe5529a7924d2b298a000
Etienne Stock <etienne.stock@web.de>
2022-11-17 15:24:58 +0100

    Removed unnecessary functions.

--- simulate.py
+++ simulate.py
@@ -5,53 +5,6 @@ import inversion
 import const
 
 
-def measure_slow(rho: np.array, N: int):
-    '''
-    Simulates several quantum measurements for a set of operators.
-
-    :param rho: state to sample from
-    :param N  : sample size
-    :return: array of N measured results and the corresponding axis
-    '''
-    axs = np.random.randint(0, high=3, size=N)
-    p1  = np.trace([3*rho@const.M_up[ax] for ax in axs], axis1=-2, axis2=-1)
-
-    if np.all(np.imag(p1)<1e-14):
-        p1 = np.real(p1)
-    else:
-        raise ValueError('Contradiction: Complex probabilities!')
-
-    p      = np.array([1-p1, p1]).T
-    choice = lambda p: np.random.choice([0, 1], p=p)
-
-    return np.array([axs, np.apply_along_axis(choice, 1, p)]).T
-
-
-def measure_unefficiently(rho: np.array, N: int):
-    '''
-    Simulates several quantum measurements for a set of operators.
-
-    :param rho: state to sample from
-    :param N  : sample size
-    :return: array of N measured results and the corresponding axis
-    '''
-    axs = np.random.randint(0, high=3, size=N)
-    ax, ax_counts = np.unique(axs, return_counts=True)
-
-    p10 = np.real(np.trace(3*rho@const.M_up[ax[0]]))
-    p11 = np.real(np.trace(3*rho@const.M_up[ax[1]]))
-    p12 = np.real(np.trace(3*rho@const.M_up[ax[2]]))
-
-    D0 = np.array([np.repeat(0, ax_counts[0]), np.random.choice([0, 1], p=[1-p10, p10], size=ax_counts[0])]).T
-    D1 = np.array([np.repeat(1, ax_counts[1]), np.random.choice([0, 1], p=[1-p11, p11], size=ax_counts[1])]).T
-    D2 = np.array([np.repeat(2, ax_counts[2]), np.random.choice([0, 1], p=[1-p12, p12], size=ax_counts[2])]).T
-
-    D = np.concatenate((D0, D1, D2), axis=0)
-    np.random.shuffle(D)
-
-    return D
-
-
 def measure(rho: np.array, N: int, M: np.array):
     '''
     Simulates several quantum measurements for set of operators.
