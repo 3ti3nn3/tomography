302aef3197e0dbbb8180b7535bcffef96034e71c
Etienne Stock <etienne.stock@web.de>
2022-11-17 16:45:49 +0100

    New class for hyperparameter alpha analysis.

--- twostep.py
+++ twostep.py
@@ -9,6 +9,7 @@ import const
 import simulate
 import visualization
 
+
 class TwoStepTomography(Tomography):
 
 
@@ -112,6 +113,7 @@ class TwoStepTomography(Tomography):
         self.logger.info(f'score of valid states: {np.sum(self._valids)/(self.N_mean*self.N_ticks)}')
 
 
+
 class TwoStepComparison(Comparison):
 
     def __init__(self, name, name_list, new=True, debug=False):
@@ -154,3 +156,143 @@ class TwoStepComparison(Comparison):
             else:
                 l.append('aligned')
         return l
+
+
+
+class AlphaDependency(Tomography):
+
+    def __init__(self, name, dim=None, N_max=None, N_mean=None, N_ticks=None, alpha_min=None, alpha_max=None, alpha_ticks=None, mirror=None, povm_name=None, f_sample=None, f_estimate=None, f_distance=None, new=True, debug=False):
+
+        self.name  = name
+        self.new   = new
+
+        # setup logging
+        self.debug = debug
+        self.setup_logging('TST - '+self.name)
+
+        # reload data
+        if self.new:
+            notNone = [dim is not None, N_max is not None, N_mean is not None, N_ticks is not None, alpha_min is not None,\
+                alpha_max is not None, alpha_ticks is not None, mirror is not None, povm_name is not None,\
+                f_sample is not None, f_estimate is not None, f_distance is not None]
+            assert all(notNone), 'Want to build up model from scratch but certain variables are not specified.'
+
+            self.logger.info('Buildung from scratch.')
+
+            self.dim         = dim
+            self.N_max       = N_max
+            self.N_mean      = N_mean
+            self.N_ticks     = N_ticks
+            self.x_N         = np.logspace(2, np.log10(self.N_max), self.N_ticks, dtype=np.int)
+            self.alpha_min   = alpha_min
+            self.alpha_max   = alpha_max
+            self.alpha_ticks = alpha_ticks
+            self.mirror      = mirror
+
+            self.povm_name  = povm_name
+            self.povm       = const.povm[self.povm_name]
+            self.f_sample   = f_sample
+            self.f_estimate = f_estimate
+            self.f_distance = f_distance
+
+            self._alpha_list = []
+            self._a          = np.zeros(self.alpha_ticks, dtype=np.float)
+            self._a_err      = np.zeros(self.alpha_ticks, dtype=np.float)
+            self._A          = np.zeros(self.alpha_ticks, dtype=np.float)
+            self._A_err      = np.zeros(self.alpha_ticks, dtype=np.float)
+        else:
+            with open('data/'+self.name+'.pt', 'rb') as file:
+                self.logger.info('Loading already existing estimation data!')
+                model = pickle.load(file)
+
+            self.dim         = model.dim
+            self.N_max       = model.N_max
+            self.N_mean      = model.N_mean
+            self.N_ticks     = model.N_ticks
+            self.x_N         = np.logspace(2, np.log10(self.N_max), self.N_ticks, dtype=np.int)
+            self.alpha_min   = model.alpha_min
+            self.alpha_max   = model.alpha_max
+            self.alpha_ticks = model.alpha_ticks
+            self.mirror      = model.mirror
+
+            self.povm_name  = model.povm_name
+            self.povm       = model.povm
+            self.f_sample   = model.f_sample
+            self.f_estimate = model.f_estimate
+            self.f_distance = model.f_estimate
+
+            self._alpha_list = model._alpha_list
+            self._a          = model._a
+            self._a_err      = model._a_err
+            self._A          = model._A
+            self._A_err      = model._A_err
+
+        # report loaded parameters
+        self.parameter_report()
+
+
+    def parameter_report(self):
+        info = '\n'\
+            'Parameter report\n'\
+            '----------------\n'\
+            f'N_max            : {self.N_max}\n'\
+            f'N_mean           : {self.N_mean}\n'\
+            f'N_ticks          : {self.N_ticks}\n'\
+            f'mirror           : {self.mirror}\n'\
+            f'alpha_min        : {self.alpha_min}\n'\
+            f'alpha_max        : {self.alpha_max}\n'\
+            f'alpha_ticks      : {self.alpha_ticks}\n'\
+            f'povm             : {self.povm_name}\n'\
+            f'sample method    : {self.f_sample.__name__}\n'\
+            f'distance measure : {self.f_distance.__name__}\n'\
+            f'estimation method: {self.f_estimate.__name__}'
+        self.logger.info(info)
+        return info
+
+
+    def get_alpha_list(self):
+        return self._alpha_list
+
+    def get_a(self):
+        return self._a
+
+    def get_A(self):
+        return self._A
+
+
+    def create_models(self):
+        for alpha in self.x_alpha:
+            name = self.name+'_'+str(alpha)
+            self._alpha_list.append(TwoStepTomography(name, self.dim, self.N_max,\
+                self.N_mean, self.N_ticks, alpha, self.mirror, self.povm_name, self.f_sample,\
+                self.f_estimate, self.f_distance, new=True, debug=self.debug))
+
+            self._alpha_list[-1].create_originals()
+            self._alpha_list[-1].reconstruct()
+
+            self.logger.info(f'Tomography model for alpha = {alpha} initialized.')
+
+
+    def extract_gradient(self):
+        for tomo, idx in enumerate(self._alpha_list):
+
+            # calculate mean
+            mean = np.mean(tomo.get_distances(), axis=0, where=tomo.get_valids())
+            std  = np.std(tomo.get_distances(), axis=0, where=tomo.get_valids())
+
+            # fit
+            try:
+                f = lambda x, a, A: A*x**a
+                popt, pcov = curve_fit(f, tomo.x_N, mean, p0=[-0.5, 0.2], sigma=std)
+
+                self._a[idx], self._A[idx] = popt
+                self._a_err = np.sqrt(pcov[0,0])
+                self._A_err = np.sqrt(popt[0,0])
+
+                self.logger.info('curve_fit successful!')
+            except:
+                 self.logger.info('curve_fit not successful!')
+
+
+    def plot_alpha_dependency(self):
+        pass
