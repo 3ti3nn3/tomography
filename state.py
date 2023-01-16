495f3c9c06958d92b468c577908faa392e4d8e9a
Etienne Stock <etienne.stock@web.de>
2023-01-16 11:42:36 +0100

    Implemented maximally mixed state for arbitrary dimensions.

--- state.py
+++ state.py
@@ -1,4 +1,5 @@
 import numpy as np
+import const
 
 
 def bell1(dim: int, N: int):
@@ -66,4 +67,37 @@ def bell4(dim: int, N: int):
                             [0, -1, 1, 0],
                             [0, 0, 0, 0]]])
     return np.repeat(bell, N, axis=0)
-                            
+
+
+def maxmixed(dim: int, N: int):
+    '''
+    Returns the maximally mixed state.
+
+    :param dim: dimension of true state
+    :param N  : number of samples
+    :return: Nxdxd array of maximally mixed state
+    '''
+    rho = np.eye(dim)/dim
+    return np.repeat(rho[None,:,:], N, axis=0)
+
+
+def purity(dim: int, N: int):
+    '''
+    Samples uniformly distributed mixed states with certain purity.
+    Only for one qubit possible.
+
+    :param dim: dimension of the state
+    :param N  : number of states
+    :return: Nxdxd or dxd array of states
+    '''
+    assert dim==2, f"Purity sampling only valid for one qubit. Dimension of current estimate: {dim}"
+    r = 0.75
+
+    x     = np.random.uniform(0, 1, size=N)
+    theta = np.arccos(1-2*x)
+    phi   = np.random.uniform(-np.pi, np.pi, size=N)
+
+    pauli = np.array([const.se, const.sx, const.sy, const.sz])
+    n     = np.array([np.ones(N), r * np.cos(theta)*np.sin(phi), r * np.sin(theta)*np.sin(phi), r * np.cos(phi)]).T
+
+    return 1/2 * np.einsum('ni, ikl->nkl', n, pauli)
